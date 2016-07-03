71ccaee06530e6c423caa2979fc6faf7437a9de6
kpa <asisten@kitapunyaacara.com>
2016-07-03 14:29:30 +0700

    change index to exist

--- applications/resources/views/front/index.blade.php
+++ applications/resources/views/front/index.blade.php
@@ -1,14 +1,155 @@
-<div class="row">
-		<div class="box">
-			<div class="col-lg-12">
-				<hr>
-				<h2 class="intro-text text-center"><strong>Comitem</strong></h2>
-				<hr>
-				<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus, et tria milia saltatricum ne interpellata quidem cum choris.</p>
-				<p>Quo cognito Constantius ultra mortalem modum exarsit ac nequo casu idem Gallus de futuris incertus agitare quaedam conducentia saluti suae per itinera conaretur, remoti sunt omnes.</p>
-				<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
-				<p>Cuius acerbitati uxor grave accesserat incentivum, germanitate Augusti turgida supra modum, quam Hannibaliano regi fratris filio antehac Constantinus iunxerat pater, Megaera quaedam mortalis, inflammatrix saevientis adsidua, humani cruoris avida nihil mitius quam maritus; qui paulatim eruditiores facti processu temporis ad nocendum per clandestinos versutosque rumigerulos conpertis leviter addere quaedam male suetos falsa et placentia sibi discentes, adfectati regni vel artium nefandarum calumnias insontibus adfligebant.</p>
-				<p>Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.</p>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Buat dan Sebarkan Undangan Pernikahan Online dan Acara Kamu Dengan Kitapunyaacara.com, Lebih Elegant dan Ekslusif, Serta Bagikan Melalui Facebook, Twitter, Whatsapp dan Line. Undangan Pernikahan Online, Undangan Event Online, Undangan Seminar Online, Undangan Pesta Online.">
+    <meta name="google-site-verification" content="OcGabK86NvekeCWlOBPU5_qkkv8You_oEznc1-F4OiE" />
+    <meta name="alexaVerifyID" content="sNV20BY-pt68eQuzqRilHFODDaA"/>
+    <meta itemprop="thumbnailUrl" content="http://kitapunyaacara.com/img/bg.jpg"/>
+    <meta itemprop="image" content="http://kitapunyaacara.com/img/bg.jpg" >
+    <meta name="keywords" content="undangan, pernikahan, online, undangan online, seminar, ulang tahun, undangan nikah, undangan pernikahan online, wedding invitation, acara, event"/>
+    <meta http-equiv="Content-Language" content="id" />
+    <meta name="author" content="kitapunyaacara">
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="all" />
+
+    <title>Kita Punya Acara | Undangan Online</title>
+
+<link href='img/kpa.ico' rel='shortcut icon' type='image/png'>
+<link href='img/favicon16px.png' rel='icon' sizes='16x16' type='image/png'>
+<link href='img/favicon32px.png' rel='icon' sizes='32x32' type='image/png'>
+
+<!-- JS -->
+<link rel="stylesheet" href="js/bootstrap.min.js" />
+
+<!-- CSS -->
+<link rel="stylesheet" href="css/main/bootstrap.min.css" />
+<link rel="stylesheet" href="css/main/font-awesome.min.css" />
+<link rel="stylesheet" href="css/main/owl.carousel.css" />
+<link rel="stylesheet" href="css/main/style6.css" />
+<link rel="stylesheet" href="css/main/custom.css" />
+
+<link rel="image_src" href="http://kitapunyaacara.com/img/bg.jpg" />
+
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-74449170-1', 'kitapunyaacara.com');
+	  ga('require', 'linkid', 'linkid.js');
+	  ga('send', 'pageview');
+     </script>
+
+</head>
+<body>
+   <div class="header">
+	<nav class="navbar space">
+	  <div class="container">
+		<div class="navbar-left">
+		  <div class="navbar-brand" href="#">
+			<a href="#">
+				<img alt="Brand" src="img/logo-kpa.png">
+			</a>
+		  </div>
+			<div class="navbar-text logo">
+			  <a href="#">
+				Kita Punya Acara
+			  </a>
 			</div>
 		</div>
+		<div class="navbar-right">
+			<a id="trigger-overlay" href="#">
+				<img alt="Brand" src="img/burger.png">
+			</a>
+		</div>
+		<div class="overlay overlay-door">
+			<button type="button" class="overlay-close">Close</button>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Work</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="clearfix"></div>
+		<div class="tagline text-center">
+			The perfect way to spread your invitation
+		</div>
+	  </div>
+	</nav>
+</div>
+
+<div class="slide">
+	<div class="container">
+        <div class="slider">
+			<img src="img/slide-one.jpg" />
+			<img src="img/slide-two.jpg" />
+        </div>
+  	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="our-work">
+	<div class="container">
+		<div class="title text-center">
+			<h1>Our Work</h1>
+		</div>
+		<div class="content-work">
+			<div class="col-our-work">
+				<a target="_blank" href="http://kitapunyaacara.com/wedding/rizaldede"><img src="img/thumb-one.jpg" /></a>
+			</div>
+			<div class="col-our-work">
+				<a target="_blank" href="http://kitapunyaacara.com/wedding/serlivicko"><img src="img/thumb-two.jpg" /></a>
+			</div>
+			<div class="col-our-work">
+				<a target="_blank" href="http://kitapunyaacara.com/wedding/rizaldede"><img src="img/thumb-one.jpg" /></a>
+			</div>
+			<div class="clearfix"></div>
+		</div>
 	</div>
+</div>
+
+<div class="footer">
+	<div class="container">
+		<div class="contact text-center">
+			<div class="title">
+				<h1>Contact Us</h1>
+			</div>
+			<div class="email">
+				Email Us at <a href="mailto:asisten@kitapunyaacara.com"> asisten@kitapunyaacara.com</a>
+			</div>
+			<div class="social">
+				Follow Us in
+				<a target="_blank" href="https://twitter.com/kitapunyaacara"> <i class="fa fa-twitter"></i> </a>
+				<a target="_blank" href="https://www.facebook.com/kitapunyaacara/"> <i class="fa fa-facebook"></i> </a>
+				<a target="_blank" href="https://www.instagram.com/kitapunyaacara/"> <i class="fa fa-instagram"></i> </a>
+			</div>
+
+		</div>
+		<div class="copy text-center">
+			Copyright &copy kitapunyaacara.com 2016
+		</div>
+	<div>
+</div>
+<script type="text/javascript" src="js/jquery-1.11.2.min.js"/></script>
+<script type="text/javascript" src="js/owl.carousel.min.js" /></script>
+<script src="js/modernizr.custom.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/demo1.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.slider').owlCarousel({
+			autoplay:!0,autoplayTimeout:2e3,animateOut:"fadeOut",loop:!0,items:1
+		})
+	});
+</script>
+</body>
+</html>
